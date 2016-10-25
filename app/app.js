21cf4bab1a4b94cff42de11520d04f47a7d146e7
Lily Jen <lily@lilyjen.com>
2016-10-24 20:40:38 -0400

    Fixes hot loading

--- app/app.js
+++ app/app.js
@@ -1,16 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Link } from 'react-router'
+import React from 'react';
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello world</h1>
-          <button><Link to="/signup">Sign Up</Link></button>
+        {this.props.children}
       </div>
-    )
+    );
   }
 }
-
-export default App 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var webpack = require('webpack');  
+
 module.exports = {  
   entry: [
+    'webpack-dev-server/client?http://0.0.0.0:8080  ',
     'webpack/hot/only-dev-server',
     "./app/index.js"
   ],
