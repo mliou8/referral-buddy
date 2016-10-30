8feae4b9527988409ef2c54015739aa80c2b7bfe
Lily Jen <lily@lilyjen.com>
2016-10-30 13:44:18 -0400

    Adds dashboard route and page

--- app/home.js
+++ app/home.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React from 'react';
+import { Link } from 'react-router';
+import Logo from './components/logo';
 
 let Home = () => (
-  <div>
-    <h1>You're not just a resume!</h1>
-    <h3>Show recruiters at Company that you’ve got the skills they want.</h3>
-    <button><Link to="/signup">Sign up now</Link></button>
+  <div className="container">
+    <Logo />
+    <div class="">
+      <h1>You're not just a resume!</h1>
+      <h3>Show recruiters at Company that you’ve got the skills they want.</h3>
+      <button><Link to="/signup">Sign up now</Link></button>
+    </div>
   </div>
 );
 
